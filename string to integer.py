e3ea82ca0625f130d0ec04075050fcdce5117905
AMAL K PHILIP <amal17philip@gmail.com>
2024-11-20 22:09:48 +0530

    string to integer.py

--- string to integer.py
+++ string to integer.py
@@ -1,6 +1,12 @@
+'''
+Author: AMAL K PHILIP
+Date:20-11-2024
+Description:program to convert string into integer.
+'''
 element=list(input("enter the character:"))
 integer=['-','0','1','2','3','4','5','6','7','8','9']
 newinteger=''
+
 for char in element:
     if char not in integer:
         break
