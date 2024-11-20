dbb91b104f1ebe461c6d9086e07cf77787830152
AMAL K PHILIP <amal17philip@gmail.com>
2024-11-20 07:19:33 +0530

    Add files via upload

---
+++ reverse number.py
@@ -0,0 +1,19 @@
+num=int(input("enter the number:"))
+
+def reverse(num):
+    newnum=0
+    re=0
+    while num>0:
+        re=num%10
+        newnum=newnum*10+re
+        num=num//10
+    return newnum
+
+if num>0:
+    newnum=reverse(num)
+
+else:
+    num=-1*num
+    newnum=reverse(num)
+    newnum=-1*newnum 
+print(newnum)
