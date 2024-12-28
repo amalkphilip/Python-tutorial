3fb92c9bfe20b6f3a162ce73e9d7003d7630c307
AMAL K PHILIP <amal17philip@gmail.com>
2024-12-28 22:22:49 +0530

    bounce distance.py

--- bounce distance.py
+++ bounce distance.py
@@ -3,12 +3,12 @@ Author:Amal K Philip
 Description:Program that let's the user enter to initial height of the ball. show the total distance the ball bounces and total bounce.
 """
 
-height=int(input("Enter the height that ball dropped(in Metre):"))
+height=int(input("Enter the height that ball dropped(in CentiMetre):"))
 distance=height
 bounce=1
-while height>2.16:
+while height>=1:
     height=height*.6
     distance+=height
     bounce+=1
-print("Total distance:",distance,"metre")
+print("Total distance:",distance,"cm")
 print("Total bounce:",bounce)
