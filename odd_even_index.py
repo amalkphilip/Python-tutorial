7fcbe47e5cf0f9e15ed6430cfc5928c2bf6c3e54
Amal K Philip <amal17philip@gmail.com>
2025-09-13 10:30:06 +0530

    Create odd_even_index.py

---
+++ odd_even_index.py
@@ -0,0 +1,21 @@
+#Author : Amal k philip
+#Odd indexed and Even indexed charaters in the String.
+    s = input("Enter the String :")
+    even_chars = ''
+    odd_chars = ''
+    
+    for i in range(len(s)):
+        if i % 2 == 0:
+            even_chars += s[i]
+        else:
+            odd_chars += s[i]
+    
+    print(f"{even_chars} {odd_chars}")
+
+
+
+"""
+Input : hello
+
+Output : hlo el
+"""
